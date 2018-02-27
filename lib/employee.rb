684dbd6daeab88a2be44e47250cd2b150b1db35b
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-27 09:43:28 -0700

    add instantiation and attribute tests for employee class.

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,10 @@
 class Employee
+  attr_reader :name
+  attr_accessor :role
+
+  def initialize(name, role)
+    @name = name
+    @role = role
+  end
+
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,8 +3,15 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
+    employee = Employee.new('Susan Smith', 'Manager')
+
+    assert_instance_of Employee, employee
   end
 
   def test_attributes
+    employee = Employee.new('Susan Smith', 'Manager')
+
+    assert_equal 'Susan Smith', employee.name
+    assert_equal 'Manager', employee.role
   end
 end

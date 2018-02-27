d5ad647b9b42161c8f7c310b50b466475125a978
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-27 09:51:50 -0700

    add more attribute tests for employee class

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,10 +1,13 @@
 class Employee
-  attr_reader :name
+  attr_reader :employee_id, :name, :start_date, :end_date
   attr_accessor :role
 
-  def initialize(name, role)
+  def initialize(employee_id, name, role, start_date, end_date)
+    @employee_id = employee_id
     @name = name
     @role = role
+    @start_date = start_date
+    @end_date = end_date
   end
 
 end
